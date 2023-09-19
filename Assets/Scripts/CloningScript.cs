7fa0f515a48082413054925ca082c1903c7eb59c
Gesporidgers <konkord302@gmail.com>
2023-09-19 21:16:43 +0300

    Clone

---
+++ Assets/Scripts/CloningScript.cs
@@ -0,0 +1,34 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            //cube = GameObject.Find("Cube_spec");
+            inst();
+        }
+    }
+    void inst()
+    {
+        for(int i=0; i < 10; i++)
+        {
+            Vector3 nv = new Vector3(i, 0, 0);
+            Instantiate(gameObject, nv,gameObject.transform.rotation);
+        }
+    }
+}
